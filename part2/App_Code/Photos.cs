e30046966b4b39461f7a90ccc904e8b0cc652191
DanielTran3 <dtran3@ualberta.ca>
2017-10-19 23:48:25 -0600

    Need to add timer callback function for changing photos

---
+++ part2/App_Code/Photos.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Photos
+/// </summary>
+public class Photos
+{
+    private List<List<string>> photoDetails;
+    private int currentPhotoIndex;
+    private bool forward;
+
+    #region Constructors
+    public Photos()
+    {
+        photoDetails = new List<List<string>>();
+        currentPhotoIndex = 0;
+        forward = true;
+    }
+    #endregion
+
+    #region Getters and Setters
+    public List<List<string>> PhotoDetails
+    {
+        get
+        {
+            return photoDetails;
+        }
+
+        set
+        {
+            this.photoDetails = value;
+        }
+    }
+
+    public int CurrentPhotoIndex
+    {
+        get
+        {
+            return currentPhotoIndex;
+        }
+
+        set
+        {
+            this.currentPhotoIndex = value;
+        }
+    }
+
+    public bool Forward
+    {
+        get
+        {
+            return forward;
+        }
+
+        set
+        {
+            this.forward = value;
+        }
+    }
+    #endregion
+
+    #region Public Methods
+    public void StorePhotoDetails(string[] photoDataLines)
+    {
+        foreach (string line in photoDataLines)
+        {
+            string[] separatedLine = line.Split(',');
+            List<string> tempList = new List<string>();
+            foreach (string s in separatedLine)
+            {
+                tempList.Add(s);
+            }
+            photoDetails.Add(tempList);
+        }
+    }
+
+    public string GetPhoto()
+    {
+        return photoDetails[currentPhotoIndex][0];
+    }
+
+    public string GetCaption()
+    {
+        return photoDetails[currentPhotoIndex][1];
+    }
+
+    public void IncrementCurrentPhotoIndex()
+    {
+        this.currentPhotoIndex++;
+    }
+
+    public void RandomCurrentPhotoIndex()
+    {
+        Random random = new Random();
+        currentPhotoIndex = random.Next(0, photoDetails.Count);
+    }
+
+    public List<string> NextPhotoAndCaption()
+    {
+        return forward ? photoDetails[currentPhotoIndex++] : photoDetails[currentPhotoIndex--];
+    }
+
+    public List<string> RandomPhotoAndCaption()
+    {
+        Random random = new Random();
+        currentPhotoIndex = random.Next(0, photoDetails.Count);
+        return photoDetails[currentPhotoIndex];
+    }
+    #endregion
+
+}
--- part2/PhotoSlideshow.aspx.cs
+++ part2/PhotoSlideshow.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,8 +8,66 @@ using System.Web.UI.WebControls;
 
 public partial class PhotoSlideshow : System.Web.UI.Page
 {
+    private Photos photos = new Photos();
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        string[] photoDataLines = File.ReadAllLines(Server.MapPath("~/App_Data/photoslideshow.txt"));
+        photos.StorePhotoDetails(photoDataLines);
+        
+    }
 
+    protected void StartStopButton_Click(object sender, EventArgs e)
+    {
+        Button button = sender as Button;
+        if (button.Text == "Start")
+        {
+            button.Text = "Stop";
+
+            // Add a timed sequential callback function
+            this.PhotoDisplay.ImageUrl = photos.GetPhoto();
+            this.CaptionLabel.Text = photos.GetCaption();
+            photos.IncrementCurrentPhotoIndex();
+        }
+        else
+        {
+            button.Text = "Start";
+        }
+    }
+
+    protected void SequentialRandomButton_Click(object sender, EventArgs e)
+    {
+        Button button = sender as Button;
+        if (button.Text == "Sequential")
+        {
+            button.Text = "Random";
+            this.ForwardBackwardButton.Enabled = false;
+
+            photos.RandomCurrentPhotoIndex();
+            this.PhotoDisplay.ImageUrl = photos.GetPhoto();
+            this.CaptionLabel.Text = photos.GetCaption();
+        }
+        else
+        {
+            button.Text = "Sequential";
+            this.ForwardBackwardButton.Enabled = true;
+        }
+    }
+
+    protected void ForwardBackwardButton_Click(object sender, EventArgs e)
+    {
+        Button button = sender as Button;
+        if (button.Text == "Forward")
+        {
+            button.Text = "Backward";
+
+            photos.Forward = true;
+        }
+        else
+        {
+            button.Text = "Forward";
+
+            photos.Forward = false;
+        }
     }
 }
