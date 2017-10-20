440287125163a3e3f6d96396f4f20f2e4dc355a8
DanielTran3 <dtran3@ualberta.ca>
2017-10-19 20:06:12 -0600

    assignment 1 (needs css work)

---
+++ part1/CookieVisits.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="CookieVisits.aspx.cs" Inherits="CookieVisits" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Cookie Visits</title>
+    <script type="text/javascript">
+        // Does the time zone need to be done in ASP.NET and C#? Or can it be done using js?
+        //window.onload = function () {
+        //    alert("load");
+        //    var offset = new Date().getTimezoneOffset();
+        //    document.getElementById("ClientTimeZoneLabel").innerHTML = offset;
+        //};
+        function test() {
+            //var offset = new Date().getTimezoneOffset() / 60;
+            //document.getElementById("ClientTimeZoneLabel").innerHTML = offset + " hours";
+            document.getElementById("ClientTimeZoneLabel").innerHTML = new Date().toUTCString();
+        }
+    </script>
+</head>
+<body onload="test()">
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="NumberOfVisitsLabel" runat="server" Text="Number of Visits: "></asp:Label>
+        <asp:Label ID="NumberOfVisitsCountLabel" runat="server"></asp:Label>
+        <br />
+        <asp:Label ID="ClientIPAddressTextLabel" runat="server" Text="IP Address: "></asp:Label>
+        <asp:Label ID="ClientIPAddressLabel" runat="server"></asp:Label>
+        <br />
+        <asp:Label ID="ClientTimeZoneTextLabel" runat="server" Text="Time Zone: "></asp:Label>
+        <asp:Label ID="ClientTimeZoneLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ part1/CookieVisits.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class CookieVisits : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //https://stackoverflow.com/questions/3140341/how-to-create-persistent-cookies-in-asp-net
+        HttpCookie visitCookie = Request.Cookies["visitCount"];
+        if (visitCookie == null)
+        {
+            visitCookie = new HttpCookie("visitCount");
+            visitCookie.Values.Add("numVisits", "0");
+            visitCookie.Expires = DateTime.Now.AddHours(24);
+            Response.Cookies.Add(visitCookie);
+        }
+        else
+        {
+            if (!string.IsNullOrEmpty(visitCookie.Values["numVisits"]))
+            {
+                int numCounts = int.Parse(visitCookie.Values["numVisits"]);
+                numCounts++;
+                visitCookie.Values.Set("numVisits", numCounts.ToString());
+                Response.Cookies.Add(visitCookie);
+            }
+        }
+        NumberOfVisitsCountLabel.Text = visitCookie.Values["numVisits"];
+        ClientIPAddressLabel.Text = GetIPAddress();
+    }
+
+    //https://stackoverflow.com/questions/735350/how-to-get-a-users-client-ip-address-in-asp-net
+    protected string GetIPAddress()
+    {
+        System.Web.HttpContext context = System.Web.HttpContext.Current;
+        string ipAddress = context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+        if (!string.IsNullOrEmpty(ipAddress))
+        {
+            string[] addresses = ipAddress.Split(',');
+            if (addresses.Length != 0)
+            {
+                return addresses[0];
+            }
+        }
+
+        return context.Request.ServerVariables["REMOTE_ADDR"];
+    }
+}
